45ab9a87dff884ab7bd9ef244c5aca86080a0ff1
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-02 10:44:45 +0700

    feat: :memo: video #6

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
+import express from "express";
+import configViewEngine from "./configs/viewEngine";
 require("dotenv").config();
 
-const express = require("express");
+// const express = require("express");
+// const path = require("path");
 
 const app = express();
 const port = process.env.port || 4000;
 
+configViewEngine(app);
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("test/index.ejs");
 });
 app.get("/about", (req, res) => {
   res.send(`I'm Nguyen`);

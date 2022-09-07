051c65d8aae428c77aa72fe045c971dc72209a6c
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-07 17:36:47 +0700

    feat: :zap: #13 complete CRUD

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,13 +1,13 @@
 import pool from "../configs/connectDB";
 
-let getHomepage = async (req, res) => {
+const getHomepage = async (req, res) => {
   const [rows, fields] = await pool.execute("SELECT * FROM users");
 
   return res.render("index.ejs", { dataUser: rows });
 };
 
 // Lấy thông tin từng id user
-let getDetailPage = async (req, res) => {
+const getDetailPage = async (req, res) => {
   let userId = req.params.id;
   let [user] = await pool.execute("SELECT * From users where id = ?", [userId]);
 
@@ -15,7 +15,7 @@ let getDetailPage = async (req, res) => {
   return res.send(JSON.stringify(user));
 };
 
-let createNewUser = async (req, res) => {
+const createNewUser = async (req, res) => {
   console.log("check req: ", req.body);
 
   // Cách 1: đối với beginner
@@ -25,7 +25,7 @@ let createNewUser = async (req, res) => {
   // let address = req.body.address;
 
   // Cách 2: dùng Destructuring của js là cú pháp giảng lượt
-  let { firstName, lastName, email, address } = req.body;
+  const { firstName, lastName, email, address } = req.body;
 
   await pool.execute(
     "INSERT INTO users(firstName, lastName, email, address) values (?,?,?,?)",
@@ -34,8 +34,32 @@ let createNewUser = async (req, res) => {
   return res.redirect("/");
 };
 
+const deleteUser = async (req, res) => {
+  let userId = req.body.userId;
+  await pool.execute("delete from users where id = ?", [userId]);
+  return res.redirect("/");
+};
+
+const getEditPage = async (req, res) => {
+  let id = req.params.id;
+  let [user] = await pool.execute("select * from users where id = ?", [id]);
+  return res.render("update.ejs", { dataUser: user[0] });
+};
+
+const postUpdateUser = async (req, res) => {
+  const { firstName, lastName, email, address, id } = req.body;
+  await pool.execute(
+    "Update users set firstName = ?, lastName = ?, email = ?, address = ? where id = ?",
+    [firstName, lastName, email, address, id]
+  );
+  console.log("check req: ", req.body);
+  return res.redirect("/");
+};
 module.exports = {
   getHomepage,
   getDetailPage,
   createNewUser,
+  deleteUser,
+  getEditPage,
+  postUpdateUser,
 };

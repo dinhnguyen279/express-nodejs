cf231ca4922fac1fca0c8f2f197d053e7a9ded9a
Nguyễn <tdinhnguyen279@gmail.com>
2022-09-10 17:19:02 +0700

    feat: :zap: #15 upload single file and multiple files

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -1,3 +1,4 @@
+import multer from "multer";
 import pool from "../configs/connectDB";
 
 const getHomepage = async (req, res) => {
@@ -55,6 +56,46 @@ const postUpdateUser = async (req, res) => {
   console.log("check req: ", req.body);
   return res.redirect("/");
 };
+
+let getUploadFile = async (req, res) => {
+  return res.render("uploadFile.ejs");
+};
+
+let handleUploadFile = async (req, res) => {
+  // req.file contains information of uploaded file
+  // req.body contains information of text fields, if there were any
+  console.log(req.file);
+  if (req.fileValidationError) {
+    return res.send(req.fileValidationError);
+  } else if (!req.file) {
+    return res.send("Please select an image to upload");
+  }
+
+  // Display uploaded image for user validation
+  res.send(
+    `You have uploaded this image: <hr/><img src="/image/${req.file.filename}" width="500"><hr /><a href="/upload">Upload another image</a>`
+  );
+};
+// Upload Multiple File
+let handleUploadMultipleFiles = async (req, res) => {
+  if (req.fileValidationError) {
+    return res.send(req.fileValidationError);
+  } else if (!req.files) {
+    return res.send("Please select an image to upload");
+  }
+  let result = "You have uploaded these images: <hr />";
+  const files = req.files;
+  console.log("check files: ", files);
+  let index, len;
+
+  //Loop through all the uploaded images and display them on frontend
+  for (index = 0, len = files.length; index < len; ++index) {
+    result += `<img src="/image/${files[index].filename}" width='300' styles="margin-right:20px;">`;
+  }
+  result += '<hr/> <a href="/upload"> Upload more images</a> ';
+  res.send(result);
+};
+
 module.exports = {
   getHomepage,
   getDetailPage,
@@ -62,4 +103,7 @@ module.exports = {
   deleteUser,
   getEditPage,
   postUpdateUser,
+  getUploadFile,
+  handleUploadFile,
+  handleUploadMultipleFiles,
 };
